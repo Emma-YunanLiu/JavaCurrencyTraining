e1299369d2c53e486cfb91b118d470ab16281e32
Emma Liu <emma.yunan.liu@kingland.com>
2023-03-11 12:33:12 +0000

    PerformanceTestingUsingImprovedTestHarness

--- src/main/java/kingland/task005/ImprovedTestHarness.java
+++ src/main/java/kingland/task005/ImprovedTestHarness.java
@@ -1,31 +1,44 @@
-package kingland.task004;
+package kingland.task005;
 
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ImprovedTestHarness {
+    private final static int INVALID_RETURN_VALUE = -1;
     public long timeTasks(int nThreads, final Runnable task) throws InterruptedException{
-        final CyclicBarrier startGate = new CyclicBarrier(1);
-        final CyclicBarrier endGate = new CyclicBarrier(nThreads);
+        final CyclicBarrier startGate = new CyclicBarrier(nThreads + 1);
+        final CyclicBarrier endGate = new CyclicBarrier(nThreads + 1);
+        final ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
 
         for(int i = 0; i < nThreads; i++) {
-            Thread t = new Thread() {
-                public void run() {
+            executorService.execute(new Thread(() -> {
+                try {
+                    startGate.await();
                     try {
-                        startGate.await();
-                        try {
-                            task.run();
-                        }finally {
-                            endGate.countDown();
-                        }
-                    } catch (InterruptedException ignored) {}
+                        task.run();
+                    } finally {
+                        endGate.await();
+                    }
+                } catch (BrokenBarrierException | InterruptedException ignored) {
+                    Thread.currentThread().interrupt();
                 }
-            };
-            t.start();
-        }
+            }));
 
-        long start = System.nanoTime();
-        startGate.countDown();
-        endGate.await();
-        long end = System.nanoTime();
-        return end-start;
+        }
+        try {
+            startGate.await();
+            long start = System.nanoTime();
+            endGate.await();
+            long end = System.nanoTime();
+            return end-start;
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+            return INVALID_RETURN_VALUE;
+        } finally {
+            executorService.shutdown();
+        }
     }
 }
